0f54a6c16d5c711a7a73a0fdfa8d9c01d63281a4
PJSM <pjsm.lesi@gmail.com>
2024-04-30 22:37:56 +0100

    Updated Program.cs

--- D14_ErrorHandling/Program.cs
+++ D14_ErrorHandling/Program.cs
@@ -16,11 +16,50 @@ namespace D14_ErrorHandling
             Utility.WriteTitle("Error handling");
 
             #region Sem tratamento de erros
+            //int number01;
 
+            //Utility.WriteMessage("Número: ");
+            //number01 = Convert.ToInt16(Console.ReadLine());
             #endregion
 
             #region Com tratamento de erros
+            try        // snippet -> Tab Tab
+            {
+                int number02;
 
+                Utility.WriteMessage("Número: ");
+                number02 = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (DivideByZeroException)
+            {
+                Utility.WriteMessage("Não podes dividir por 0", "", "\n");
+            }
+            catch (FormatException ex)
+            {
+                Utility.WriteMessage("Escreve um número", "", "\n");
+            }
+            catch (Exception ex)        // variável do tipo Exception (ex -> nome standard)
+            {
+                #region Versão de desenolvimento
+                Utility.WriteMessage($"Erro: {ex.Message}", "\n");        // imprime o erro sem ter de fazer throw (terminar)
+                                                                                //throw;        // lançar o erro e interrompe a execução
+                #endregion
+
+                #region Versão de produção (devemos implementar um sistema de logs)
+                //Utility.WriteMessage("Aconteceu um erro", "", "\n");        // versão de produção -> devemos implementar um sistema de logs
+                #endregion
+            }
+            // os catchs têm de estar em linhas consecutivas (sem \n's)
+            // usar os catchs mais específicos em cima
+            // usar o catch mais geral em baixo (catch all)
+            finally     // é opcional mas se existir é SEMPRE executado
+            {
+                Utility.WriteMessage("Terminei!", "", "\n"); 
+            }
+            // útil para usar em DataBases para a fechar, quer dê erro, ou não
+
+            // event viewer do Windows -> permite ver os eventos que estão a acontecer no PC
+            // Windows -> Event Viewer
             #endregion
 
             Utility.TerminateConsole();
